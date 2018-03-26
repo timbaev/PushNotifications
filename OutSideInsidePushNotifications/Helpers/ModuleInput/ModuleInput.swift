bed94ced26baa30fb11c2590e3d107b192ab7e85
Timbaev <hometim55@gmail.com>
2018-03-27 00:03:58 +0300

    Add gitignore and prepare project

---
+++ OutSideInsidePushNotifications/Helpers/ModuleInput/ModuleInput.swift
@@ -0,0 +1,9 @@
+//
+//  ModuleInput.swift
+//  OutSideInsidePushNotifications
+//
+//  Created by Тимур Шафигуллин on 26.03.18.
+//  Copyright © 2018 iOSLab. All rights reserved.
+//
+
+import Foundation
---
+++ OutSideInsidePushNotifications/Helpers/ModuleInput/ModuleInputHolder.swift
@@ -0,0 +1,9 @@
+//
+//  ModuleInputHolder.swift
+//  OutSideInsidePushNotifications
+//
+//  Created by Тимур Шафигуллин on 26.03.18.
+//  Copyright © 2018 iOSLab. All rights reserved.
+//
+
+import Foundation
